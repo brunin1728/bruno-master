861399139394d5e6b2815d0e95cb11083c8618d7
Bruno Carvalho <brunocarvalho.info@gmail.com>
2019-08-26 10:00:12 -0300

    master

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ApiProvider } from '../../providers/api/api';
-
+import swal from 'sweetalert';
 
 
 @IonicPage()
