1ffc06a26be05f8aebf589c89b2ad73d969362eb
Bruno Carvalho <brunocarvalho.info@gmail.com>
2019-09-09 21:28:37 -0300

    casa

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ExpandableionicComponent } from './expandableionic/expandableionic';
+@NgModule({
+	declarations: [ExpandableionicComponent],
+	imports: [],
+	exports: [ExpandableionicComponent]
+})
+export class ComponentsModule {}
